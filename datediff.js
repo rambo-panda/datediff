afa90066ae13dcdf9c671d421e9a72ffde336b99
rambo-panda <liluwei123@163.com>
2016-03-21 16:30:19 +0800

    Update datediff.js
    floor second

--- datediff.js
+++ datediff.js
@@ -2,7 +2,7 @@
 
 /*global define:true */
 
-(function( factory ) {
+(function(factory) {
 
 	factory = factory();
 
@@ -11,7 +11,7 @@
 	}else if ((typeof module !== 'undefined' && module.exports)){
 		module.exports = factory;
 	}else{
-		this.date_diff = factory;
+		window.date_diff = factory;
 	}
 
 }(function() {
@@ -66,7 +66,6 @@
 		*/
 		date_diff = function(ms, max_unit) {
 
-
 			var floor = Math.floor,
 				seconds = ms / 1000,
 				hour_s = 3600,
@@ -86,7 +85,7 @@
 				d : floor(format_days - (format_month * 30)),
 				h : floor(rest_hour_s / 3600),
 				m : floor(rest_min_s / 60),
-				s : rest_min_s % 60
+				s : floor(rest_min_s % 60)
 			};
 
 			max_unit && date_diff_format(normal_result, max_unit);
@@ -94,44 +93,6 @@
 			return  normal_result;
 
 		};
-
-
-	//new Date('2017/6/5 10:20') -  new Date('2015/5/12 5:10')
-	console.assert(
-		JSON.stringify(date_diff(65250600000)) === '{"y":2,"M":0,"d":24,"h":17,"m":32,"s":28}',
-		'正常月份'
-	);
-
-	// new Date('2016/5/5') -  new Date('2015/5/5')
-	console.assert(
-		JSON.stringify(date_diff(31622400000, 'y')) === '{"y":1,"M":0,"d":0,"h":18,"m":11,"s":14}',
-		'年判断出错'
-	);
-
-	console.assert(
-		JSON.stringify(date_diff(31536003000, 'M')) === '{"y":0,"M":11,"d":35,"h":0,"m":0,"s":3}',
-		'月判断出错'
-	);
-
-	console.assert(
-		JSON.stringify(date_diff(31536003000, 'd')) === '{"y":0,"M":0,"d":369,"h":0,"m":0,"s":3}',
-		'天判断出错'
-	);
-
-	console.assert(
-		JSON.stringify(date_diff(31536003000, 'h')) === '{"y":0,"M":0,"d":0,"h":9211,"m":0,"s":3}',
-		'小时判断出错'
-	);
-
-	console.assert(
-		JSON.stringify(date_diff(31536003000, 'm')) === '{"y":0,"M":0,"d":0,"h":0,"m":561871,"s":3}',
-		'分钟判断出错'
-	);
-
-	console.assert(
-		JSON.stringify(date_diff(31536003000, 's')) === '{"y":0,"M":0,"d":0,"h":0,"m":0,"s":34274134}',
-		'秒判断出错'
-	);
 
 	return date_diff;
 
